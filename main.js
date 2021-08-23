dacb09cabdedad3fdcc06d5e1af9bca8aa02a876
AlaaZAhmed <88758855+AlaaZAhmed@users.noreply.github.com>
2021-08-23 12:55:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+/*
+var btn = document.getElementById("done");
+
+
+function age(){
+    let ageBtn = document.getElementById("age").value;
+    console.log(ageBtn);
+    if(age>14){
+        btn.visibility= 'visable';
+    }
+}*/
